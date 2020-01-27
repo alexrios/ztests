c9a93308d687c8281a6e322773d7bb5aa5ed1f46
Alex Rios <alex.rios@protonmail.com>
2020-01-27 19:48:36 -0300

    setup with timeout

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/alexrios/ztests
+
+go 1.13
+
+require github.com/alexrios/canned-containers v0.0.0-20190810211714-729bcb29d387
--- postgres/db.go
+++ postgres/db.go
@@ -4,10 +4,28 @@ import (
 	"database/sql"
 	"github.com/alexrios/canned-containers/databases/postgres"
 	"testing"
+	"time"
 )
 
 func Setup(t *testing.T) (db *sql.DB, teardown func()) {
 	dbCtx, err := postgresdb.DefaultPostgresContainer().CreateContainerContext()
+	if err != nil {
+		t.Fatal(err)
+	}
+	return dbCtx.Conn, func() {
+		if dbCtx.Conn != nil {
+			_ = dbCtx.Conn.Close()
+		}
+		if dbCtx.Ctx != nil {
+			_ = dbCtx.Container.Terminate(dbCtx.Ctx)
+		}
+	}
+}
+
+func SetupWithTimeout(t *testing.T, timeout time.Duration) (db *sql.DB, teardown func()) {
+	container := postgresdb.DefaultPostgresContainer()
+	container.WithTimeout(timeout)
+	dbCtx, err := container.CreateContainerContext()
 	if err != nil {
 		t.Fatal(err)
 	}
