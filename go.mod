5761b9442f7b99109cfc862a0b3e39dfea1f243a
Alex Rios <alex.rios@protonmail.com>
2020-09-15 16:41:13 -0300

    add migrate v4

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/alexrios/ztests
 go 1.13
 
 require (
-	github.com/alexrios/canned-containers v0.1.0
+	github.com/alexrios/canned-containers v0.2.2
+	github.com/golang-migrate/migrate/v4 v4.12.2
 	github.com/jackc/pgx/v4 v4.6.0
 )
--- postgres/db.go
+++ postgres/db.go
@@ -3,6 +3,9 @@ package postgres
 import (
 	"database/sql"
 	"github.com/alexrios/canned-containers/databases/postgres"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"testing"
 	"time"
 )
@@ -22,6 +25,32 @@ func Setup(t *testing.T) (db *sql.DB, teardown func()) {
 	}
 }
 
+func SetupAndMigrate(t *testing.T, migrationPath string) (db *sql.DB, teardown func()) {
+	dbCtx, err := postgresdb.DefaultPostgresContainer().CreateContainerContext()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var mig *migrate.Migrate
+	mig, err = migrate.New("file://"+migrationPath, dbCtx.ConnStr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err = mig.Up(); err != nil {
+		t.Fatal(err)
+	}
+
+	return dbCtx.Conn, func() {
+		_ = mig.Down()
+		if dbCtx.Conn != nil {
+			_ = dbCtx.Conn.Close()
+		}
+		if dbCtx.Ctx != nil {
+			_ = dbCtx.Container.Terminate(dbCtx.Ctx)
+		}
+	}
+}
+
 func SetupWithTimeout(t *testing.T, timeout time.Duration) (db *sql.DB, teardown func()) {
 	container := postgresdb.DefaultPostgresContainer()
 	container.WithTimeout(timeout)
@@ -30,6 +59,34 @@ func SetupWithTimeout(t *testing.T, timeout time.Duration) (db *sql.DB, teardown
 		t.Fatal(err)
 	}
 	return dbCtx.Conn, func() {
+		if dbCtx.Conn != nil {
+			_ = dbCtx.Conn.Close()
+		}
+		if dbCtx.Ctx != nil {
+			_ = dbCtx.Container.Terminate(dbCtx.Ctx)
+		}
+	}
+}
+
+func SetupAndMigrateWithTimeout(t *testing.T, migrationPath string, timeout time.Duration) (db *sql.DB, teardown func()) {
+	container := postgresdb.DefaultPostgresContainer()
+	container.WithTimeout(timeout)
+	dbCtx, err := container.CreateContainerContext()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var mig *migrate.Migrate
+	mig, err = migrate.New("file://"+migrationPath, dbCtx.ConnStr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err = mig.Up(); err != nil {
+		t.Fatal(err)
+	}
+
+	return dbCtx.Conn, func() {
+		_ = mig.Down()
 		if dbCtx.Conn != nil {
 			_ = dbCtx.Conn.Close()
 		}
