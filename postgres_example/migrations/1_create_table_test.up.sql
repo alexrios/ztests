dece2b117f7afdc82f82f1109ee7fda91923a6da
Alex Rios <alex.rios@protonmail.com>
2020-09-16 23:46:49 -0300

    fix migrate v4 support

---
+++ postgres_example/migrations/1_create_table_test.up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE test
+(
+    column1 SERIAL primary key,
+    column2 integer not null
+);
--- postgres_example/pool_test.go
+++ postgres_example/pool_test.go
@@ -9,19 +9,39 @@ import (
 
 func TestPool(t *testing.T) {
 	ctx := context.Background()
-	testEnv, err := postgres.SetupPGX()
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer testEnv.Teardown()
+	t.Run("default setup", func(t *testing.T) {
+		testEnv, err := postgres.SetupPGX()
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer testEnv.Teardown()
 
-	var now time.Time
-	row := testEnv.Pool.QueryRow(ctx, "select now()")
-	err = row.Scan(&now)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if time.Now().IsZero() {
-		t.FailNow()
-	}
+		var now time.Time
+		row := testEnv.Pool.QueryRow(ctx, "select now()")
+		err = row.Scan(&now)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if time.Now().IsZero() {
+			t.FailNow()
+		}
+	})
+	t.Run("setup with migration", func(t *testing.T) {
+		testEnv, err := postgres.SetupPGX(postgres.Migrate("migrations"))
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer testEnv.Teardown()
+
+		var id int64
+		row := testEnv.Pool.QueryRow(ctx, "insert into test (column2) values (13) returning column1")
+		err = row.Scan(&id)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if id == 0 {
+			t.FailNow()
+		}
+	})
+
 }
